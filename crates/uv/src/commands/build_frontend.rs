f241183419837f5a907cf4ea51cf4e72808947b1
Zanie Blue <contact@zanie.dev>
2026-07-14 13:32:41 -0500

    Use an enum for the build mode

--- crates/uv/src/commands/build_frontend.rs
+++ crates/uv/src/commands/build_frontend.rs
@@ -49,7 +49,7 @@ use crate::commands::pip::operations;
 use crate::commands::project::{ProjectError, find_requires_python};
 use crate::commands::reporters::PythonDownloadReporter;
 use crate::printer::Printer;
-use crate::settings::{BuildOutputSelection, BuildPackageSelection, ResolverSettings};
+use crate::settings::{BuildMode, BuildOutputSelection, BuildPackageSelection, ResolverSettings};
 
 #[derive(Debug, Error)]
 pub(crate) enum Error {
@@ -81,8 +81,6 @@ pub(crate) enum Error {
     Project(#[from] Box<ProjectError>),
     #[error("Failed to write message")]
     Fmt(#[from] fmt::Error),
-    #[error("Can't use `--force-pep517` with `--list`")]
-    ListForcePep517,
     #[error(
         "Can only use `--list` with a compatible uv build backend, but `{name}` is not compatible because {reason}"
     )]
@@ -194,10 +192,9 @@ pub(crate) async fn build_frontend(
     package: BuildPackageSelection,
     output_dir: Option<PathBuf>,
     output: BuildOutputSelection,
-    list: bool,
+    mode: BuildMode,
     build_logs: bool,
     gitignore: bool,
-    force_pep517: bool,
     clear: bool,
     build_constraints: Vec<RequirementsSource>,
     build_constraints_from_workspace: Vec<NameRequirementSpecification>,
@@ -221,10 +218,9 @@ pub(crate) async fn build_frontend(
         &package,
         output_dir.as_deref(),
         output,
-        list,
+        mode,
         build_logs,
         gitignore,
-        force_pep517,
         clear,
         &build_constraints,
         &build_constraints_from_workspace,
@@ -268,10 +264,9 @@ async fn build_impl(
     package: &BuildPackageSelection,
     output_dir: Option<&Path>,
     output: BuildOutputSelection,
-    list: bool,
+    mode: BuildMode,
     build_logs: bool,
     gitignore: bool,
-    force_pep517: bool,
     clear: bool,
     build_constraints: &[RequirementsSource],
     build_constraints_from_workspace: &[NameRequirementSpecification],
@@ -473,7 +468,6 @@ async fn build_impl(
             hash_checking,
             build_logs,
             gitignore,
-            force_pep517,
             clear,
             build_constraints,
             build_constraints_from_workspace,
@@ -487,7 +481,7 @@ async fn build_impl(
             concurrency,
             build_options,
             output,
-            list,
+            mode,
             dependency_metadata,
             *link_mode,
             config_setting,
@@ -548,7 +542,6 @@ async fn build_package(
     hash_checking: Option<HashCheckingMode>,
     build_logs: bool,
     gitignore: bool,
-    force_pep517: bool,
     clear: bool,
     build_constraints: &[RequirementsSource],
     build_constraints_from_workspace: &[NameRequirementSpecification],
@@ -562,7 +555,7 @@ async fn build_package(
     concurrency: &Concurrency,
     build_options: &BuildOptions,
     output: BuildOutputSelection,
-    list: bool,
+    mode: BuildMode,
     dependency_metadata: &DependencyMetadata,
     link_mode: LinkMode,
     config_setting: &ConfigSettings,
@@ -723,28 +716,24 @@ async fn build_package(
 
     // Check if the build backend is matching uv version that allows calling in the uv build backend
     // directly.
-    let build_action = if list {
-        if force_pep517 {
-            return Err(Error::ListForcePep517);
-        }
+    let build_action = match mode {
+        BuildMode::List => {
+            if let Err(reason) = check_direct_build(
+                source.path(),
+                uv_version::version(),
+                &interpreter.to_resolver_marker_environment(),
+                build_constraints.requirements().cloned().map(Into::into),
+            ) {
+                return Err(Error::ListNonUv {
+                    name: source.path().user_display().to_string(),
+                    reason: reason.to_string(),
+                });
+            }
 
-        if let Err(reason) = check_direct_build(
-            source.path(),
-            uv_version::version(),
-            &interpreter.to_resolver_marker_environment(),
-            build_constraints.requirements().cloned().map(Into::into),
-        ) {
-            return Err(Error::ListNonUv {
-                name: source.path().user_display().to_string(),
-                reason: reason.to_string(),
-            });
+            BuildAction::List
         }
-
-        BuildAction::List
-    } else if force_pep517 {
-        BuildAction::Pep517
-    } else {
-        match check_direct_build(
+        BuildMode::Pep517 => BuildAction::Pep517,
+        BuildMode::Build => match check_direct_build(
             source.path(),
             uv_version::version(),
             &interpreter.to_resolver_marker_environment(),
@@ -759,7 +748,7 @@ async fn build_package(
                 );
                 BuildAction::Pep517
             }
-        }
+        },
     };
 
     if matches!(build_action, BuildAction::DirectBuild | BuildAction::List) {
@@ -790,7 +779,7 @@ async fn build_package(
         BuildPlan::SdistToWheel => {
             // Even when listing files, we still need to build the source distribution for the wheel
             // build.
-            if list {
+            if matches!(mode, BuildMode::List) {
                 let sdist_list = build_sdist(
                     source.path(),
                     &output_dir,
