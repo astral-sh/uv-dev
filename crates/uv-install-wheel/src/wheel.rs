zaniebot/uv

Reject symlinked package directories during wheel install

crates/uv-install-wheel/src/wheel.rs
@@ -1105,20 +1105,33 @@ impl RenameOrCopy {
     /// Usually, source and target are on the same device, so we can rename, but if that fails, we
     /// have to copy. If renaming failed once, we switch to copy permanently.
     fn rename_or_copy(&mut self, from: impl AsRef<Path>, to: impl AsRef<Path>) -> io::Result<()> {
+        let from = from.as_ref();
+        let to = to.as_ref();
+
         match self {
-            Self::Rename => match fs_err::rename(from.as_ref(), to.as_ref()) {
+            Self::Rename => match fs_err::rename(from, to) {
                 Ok(()) => {}
                 Err(err) => {
                     *self = Self::Copy;
                     debug!("Failed to rename, falling back to copy: {err}");
-                    fs_err::copy(from.as_ref(), to.as_ref())?;
+                    Self::copy(from, to)?;
                 }
             },
             Self::Copy => {
-                fs_err::copy(from.as_ref(), to.as_ref())?;
+                Self::copy(from, to)?;
             }
         }
         Ok(())
+    }
+
+    /// Copy through a temporary file so an existing destination symlink is replaced, not followed.
+    fn copy(from: &Path, to: &Path) -> io::Result<()> {
+        let parent = to.parent().ok_or_else(|| {
+            io::Error::new(io::ErrorKind::InvalidInput, "Destination has no parent")
+        })?;
+        let tempfile = uv_fs::tempfile_in(parent)?;
+        fs_err::copy(from, tempfile.path())?;
+        persist_with_retry_sync(tempfile, to)
     }
 }
 
@@ -1131,10 +1144,39 @@ mod test {
     use assert_fs::prelude::*;
     use indoc::{formatdoc, indoc};
 
+    #[cfg(unix)]
+    use super::RenameOrCopy;
     use super::{
         Error, RecordEntry, Script, WheelFile, format_shebang, get_script_executable,
         parse_email_message_file, parse_scripts, read_record, write_installer_metadata,
     };
+
+    #[cfg(unix)]
+    #[test]
+    fn rename_or_copy_does_not_follow_destination_symlink() -> Result<()> {
+        let temp_dir = assert_fs::TempDir::new()?;
+        let source = temp_dir.child("source");
+        let destination = temp_dir.child("destination");
+        let external = temp_dir.child("external");
+        source.write_str("wheel payload")?;
+        external.write_str("external contents")?;
+        std::os::unix::fs::symlink(external.path(), destination.path())?;
+
+        RenameOrCopy::Copy.rename_or_copy(source.path(), destination.path())?;
+
+        assert_eq!(
+            fs_err::read_to_string(external.path())?,
+            "external contents"
+        );
+        assert_eq!(fs_err::read_to_string(destination.path())?, "wheel payload");
+        assert!(
+            !fs_err::symlink_metadata(destination.path())?
+                .file_type()
+                .is_symlink()
+        );
+
+        Ok(())
+    }
 
     #[test]
     fn test_parse_email_message_file() {

crates/uv/tests/pip_install/pip_install.rs
@@ -14,7 +14,7 @@ use fs_err::File;
 use futures::executor::block_on;
 use futures::io::AllowStdIo;
 use indoc::{formatdoc, indoc};
-use insta::assert_snapshot;
+use insta::{allow_duplicates, assert_snapshot};
 use predicates::prelude::predicate;
 use tokio_util::compat::{FuturesAsyncReadCompatExt, FuturesAsyncWriteCompatExt};
 use url::Url;
@@ -14149,6 +14149,123 @@ fn reserved_script_name() -> Result<()> {
       Caused by: Scripts must not use the reserved name `python`, got: `python`
     "
     );
+
+    Ok(())
+}
+
+/// Wheel installation must not merge package files through a pre-existing directory symlink.
+#[cfg(unix)]
+#[test]
+fn reject_symlinked_wheel_package_directory() -> Result<()> {
+    let context = uv_test::test_context!("3.12");
+
+    context.init().arg("--lib").arg("foo").assert().success();
+    context.build().arg("--wheel").arg("foo").assert().success();
+    let wheel = context.temp_dir.join("foo/dist/foo-0.1.0-py3-none-any.whl");
+
+    let external = context.temp_dir.child("external");
+    external.create_dir_all()?;
+    external.child("sentinel.txt").write_str("keep me")?;
+    std::os::unix::fs::symlink(external.path(), context.site_packages().join("foo"))?;
+
+    uv_snapshot!(context.filters(), context.pip_install()
+        .arg("--link-mode")
+        .arg("copy")
+        .arg(&wheel), @"
+    success: false
+    exit_code: 2
+    ----- stdout -----
+
+    ----- stderr -----
+    Resolved 1 package in [TIME]
+    Prepared 1 package in [TIME]
+    error: Failed to install: foo-0.1.0-py3-none-any.whl (foo==0.1.0 (from file://[TEMP_DIR]/foo/dist/foo-0.1.0-py3-none-any.whl))
+      Caused by: The wheel is invalid: Cannot install into symlinked directory: [SITE_PACKAGES]/foo
+    ");
+
+    external
+        .child("sentinel.txt")
+        .assert(predicate::path::is_file());
+    external
+        .child("__init__.py")
+        .assert(predicate::path::missing());
+
+    Ok(())
+}
+
+/// Wheel data must not merge purelib, platlib, or data files through a directory symlink.
+#[cfg(unix)]
+#[test]
+fn reject_symlinked_wheel_data_package_directory() -> Result<()> {
+    allow_duplicates! {
+        for data_type in ["purelib", "platlib", "data"] {
+            let context = uv_test::test_context!("3.12");
+            let wheel = context.temp_dir.join("foo-0.1.0-py3-none-any.whl");
+            let data_path = if data_type == "data" {
+                let site_packages = context.site_packages();
+                let relative = site_packages.strip_prefix(context.venv.path())?;
+                format!(
+                    "foo-0.1.0.data/data/{}/foo/__init__.py",
+                    PortablePath::from(relative)
+                )
+            } else {
+                format!("foo-0.1.0.data/{data_type}/foo/__init__.py")
+            };
+            let record = formatdoc! {"
+                foo-0.1.0.dist-info/METADATA,,
+                foo-0.1.0.dist-info/WHEEL,,
+                foo-0.1.0.dist-info/RECORD,,
+                {data_path},,
+            "};
+
+            let mut writer = ZipFileWriter::new(Vec::new());
+            for (name, contents) in [
+                (
+                    "foo-0.1.0.dist-info/METADATA",
+                    "Metadata-Version: 2.1\nName: foo\nVersion: 0.1.0\n",
+                ),
+                (
+                    "foo-0.1.0.dist-info/WHEEL",
+                    "Wheel-Version: 1.0\nRoot-Is-Purelib: true\nTag: py3-none-any\n",
+                ),
+                ("foo-0.1.0.dist-info/RECORD", record.as_str()),
+                (data_path.as_str(), "VALUE = 1\n"),
+            ] {
+                let entry = ZipEntryBuilder::new(name.into(), Compression::Stored);
+                block_on(writer.write_entry_whole(entry, contents.as_bytes()))?;
+            }
+            fs_err::write(&wheel, block_on(writer.close())?)?;
+
+            let external = context.temp_dir.child("external");
+            external.create_dir_all()?;
+            external.child("sentinel.txt").write_str("keep me")?;
+            std::os::unix::fs::symlink(external.path(), context.site_packages().join("foo"))?;
+
+            uv_snapshot!(context.filters(), context.pip_install()
+            .arg("--link-mode")
+            .arg("copy")
+            .arg(&wheel), @"
+        success: false
+        exit_code: 2
+        ----- stdout -----
+
+        ----- stderr -----
+        Resolved 1 package in [TIME]
+        Prepared 1 package in [TIME]
+        error: Failed to install: foo-0.1.0-py3-none-any.whl (foo==0.1.0 (from file://[TEMP_DIR]/foo-0.1.0-py3-none-any.whl))
+          Caused by: The wheel is invalid: Cannot install into symlinked directory: [SITE_PACKAGES]/foo
+            ");
+
+            external
+                .child("sentinel.txt")
+                .assert(predicate::path::is_file());
+            external
+                .child("__init__.py")
+                .assert(predicate::path::missing());
+            assert!(!context.site_packages().join("foo-0.1.0.dist-info").exists());
+        }
+        Ok::<(), anyhow::Error>(())
+    }?;
 
     Ok(())
 }

astral-sh/uv-dev

crates/uv-install-wheel/src/wheel.rs
@@ -1200,20 +1200,33 @@ impl RenameOrCopy {
     /// Usually, source and target are on the same device, so we can rename, but if that fails, we
     /// have to copy. If renaming failed once, we switch to copy permanently.
     fn rename_or_copy(&mut self, from: impl AsRef<Path>, to: impl AsRef<Path>) -> io::Result<()> {
+        let from = from.as_ref();
+        let to = to.as_ref();
+
         match self {
-            Self::Rename => match fs_err::rename(from.as_ref(), to.as_ref()) {
+            Self::Rename => match fs_err::rename(from, to) {
                 Ok(()) => {}
                 Err(err) => {
                     *self = Self::Copy;
                     debug!("Failed to rename, falling back to copy: {err}");
-                    fs_err::copy(from.as_ref(), to.as_ref())?;
+                    Self::copy(from, to)?;
                 }
             },
             Self::Copy => {
-                fs_err::copy(from.as_ref(), to.as_ref())?;
+                Self::copy(from, to)?;
             }
         }
         Ok(())
+    }
+
+    /// Copy through a temporary file so an existing destination symlink is replaced, not followed.
+    fn copy(from: &Path, to: &Path) -> io::Result<()> {
+        let parent = to.parent().ok_or_else(|| {
+            io::Error::new(io::ErrorKind::InvalidInput, "Destination has no parent")
+        })?;
+        let tempfile = uv_fs::tempfile_in(parent)?;
+        fs_err::copy(from, tempfile.path())?;
+        persist_with_retry_sync(tempfile, to)
     }
 }
 
@@ -1226,10 +1239,39 @@ mod test {
     use assert_fs::prelude::*;
     use indoc::{formatdoc, indoc};
 
+    #[cfg(unix)]
+    use super::RenameOrCopy;
     use super::{
         Error, RecordEntry, Script, WheelFile, format_shebang, get_script_executable,
         parse_email_message_file, parse_scripts, read_record, write_installer_metadata,
     };
+
+    #[cfg(unix)]
+    #[test]
+    fn rename_or_copy_does_not_follow_destination_symlink() -> Result<()> {
+        let temp_dir = assert_fs::TempDir::new()?;
+        let source = temp_dir.child("source");
+        let destination = temp_dir.child("destination");
+        let external = temp_dir.child("external");
+        source.write_str("wheel payload")?;
+        external.write_str("external contents")?;
+        fs_err::os::unix::fs::symlink(external.path(), destination.path())?;
+
+        RenameOrCopy::Copy.rename_or_copy(source.path(), destination.path())?;
+
+        assert_eq!(
+            fs_err::read_to_string(external.path())?,
+            "external contents"
+        );
+        assert_eq!(fs_err::read_to_string(destination.path())?, "wheel payload");
+        assert!(
+            !fs_err::symlink_metadata(destination.path())?
+                .file_type()
+                .is_symlink()
+        );
+
+        Ok(())
+    }
 
     #[test]
     fn test_parse_email_message_file() {

crates/uv/tests/pip_install/pip_install.rs
@@ -12807,6 +12807,117 @@ fn reserved_script_name() -> Result<()> {
     Ok(())
 }
 
+/// Wheel installation must not merge package files through a pre-existing directory symlink.
+#[cfg(unix)]
+#[test]
+fn reject_symlinked_wheel_package_directory() -> Result<()> {
+    let context = uv_test::test_context!("3.12");
+
+    context.init().arg("--lib").arg("foo").assert().success();
+    context.build().arg("--wheel").arg("foo").assert().success();
+    let wheel = context.temp_dir.join("foo/dist/foo-0.1.0-py3-none-any.whl");
+
+    let external = context.temp_dir.child("external");
+    external.create_dir_all()?;
+    external.child("sentinel.txt").write_str("keep me")?;
+    fs_err::os::unix::fs::symlink(external.path(), context.site_packages().join("foo"))?;
+
+    uv_snapshot!(context.filters(), context.pip_install()
+        .arg("--link-mode")
+        .arg("copy")
+        .arg(&wheel), @"
+    exit_code: 2 (failure)
+    ----- stderr -----
+    Resolved 1 package in [TIME]
+    Prepared 1 package in [TIME]
+    error: Failed to install: foo-0.1.0-py3-none-any.whl (foo==0.1.0 (from file://[TEMP_DIR]/foo/dist/foo-0.1.0-py3-none-any.whl))
+      Caused by: The wheel is invalid: Cannot install into symlinked directory: [SITE_PACKAGES]/foo
+    ");
+
+    external
+        .child("sentinel.txt")
+        .assert(predicate::path::is_file());
+    external
+        .child("__init__.py")
+        .assert(predicate::path::missing());
+
+    Ok(())
+}
+
+/// Wheel data must not merge purelib, platlib, or data files through a directory symlink.
+#[cfg(unix)]
+#[test]
+fn reject_symlinked_wheel_data_package_directory() -> Result<()> {
+    insta::allow_duplicates! {
+        for data_type in ["purelib", "platlib", "data"] {
+            let context = uv_test::test_context!("3.12");
+            let wheel = context.temp_dir.join("foo-0.1.0-py3-none-any.whl");
+            let data_path = if data_type == "data" {
+                let site_packages = context.site_packages();
+                let relative = site_packages.strip_prefix(context.venv.path())?;
+                format!(
+                    "foo-0.1.0.data/data/{}/foo/__init__.py",
+                    PortablePath::from(relative)
+                )
+            } else {
+                format!("foo-0.1.0.data/{data_type}/foo/__init__.py")
+            };
+            let record = formatdoc! {"
+                foo-0.1.0.dist-info/METADATA,,
+                foo-0.1.0.dist-info/WHEEL,,
+                foo-0.1.0.dist-info/RECORD,,
+                {data_path},,
+            "};
+
+            let mut writer = ZipFileWriter::new(Vec::new());
+            for (name, contents) in [
+                (
+                    "foo-0.1.0.dist-info/METADATA",
+                    "Metadata-Version: 2.1\nName: foo\nVersion: 0.1.0\n",
+                ),
+                (
+                    "foo-0.1.0.dist-info/WHEEL",
+                    "Wheel-Version: 1.0\nRoot-Is-Purelib: true\nTag: py3-none-any\n",
+                ),
+                ("foo-0.1.0.dist-info/RECORD", record.as_str()),
+                (data_path.as_str(), "VALUE = 1\n"),
+            ] {
+                let entry = ZipEntryBuilder::new(name.into(), Compression::Stored);
+                block_on(writer.write_entry_whole(entry, contents.as_bytes()))?;
+            }
+            fs_err::write(&wheel, block_on(writer.close())?)?;
+
+            let external = context.temp_dir.child("external");
+            external.create_dir_all()?;
+            external.child("sentinel.txt").write_str("keep me")?;
+            fs_err::os::unix::fs::symlink(external.path(), context.site_packages().join("foo"))?;
+
+            uv_snapshot!(context.filters(), context.pip_install()
+            .arg("--link-mode")
+            .arg("copy")
+            .arg(&wheel), @"
+        exit_code: 2 (failure)
+        ----- stderr -----
+        Resolved 1 package in [TIME]
+        Prepared 1 package in [TIME]
+        error: Failed to install: foo-0.1.0-py3-none-any.whl (foo==0.1.0 (from file://[TEMP_DIR]/foo-0.1.0-py3-none-any.whl))
+          Caused by: The wheel is invalid: Cannot install into symlinked directory: [SITE_PACKAGES]/foo
+            ");
+
+            external
+                .child("sentinel.txt")
+                .assert(predicate::path::is_file());
+            external
+                .child("__init__.py")
+                .assert(predicate::path::missing());
+            assert!(!context.site_packages().join("foo-0.1.0.dist-info").exists());
+        }
+        Ok::<(), anyhow::Error>(())
+    }?;
+
+    Ok(())
+}
+
 #[test]
 fn reject_reserved_wheel_data_script_name() -> Result<()> {
     let interpreter = if cfg!(windows) {