zaniebot/uv

Percent-encode Git references in GitHub API requests
## Summary

Prior to this change, Git references containing URL delimiters were interpolated directly into the GitHub commits API URL. For example, `feature/foo#bar%baz` queried only `.../commits/feature/foo` because `#bar%baz` became a URL fragment.

This commit uses the existing URL-encoded Git reference when constructing the GitHub API request. The fast path now queries `.../commits/feature/foo%23bar%25baz`, with integration coverage asserting the endpoint is requested exactly once.

crates/uv-git/src/resolver.rs
@@ -121,11 +121,14 @@ impl GitResolver {
         }
 
         // Determine the Git reference.
-        let rev = url.reference().as_rev();
+        let reference = url
+            .reference()
+            .as_url_rev()
+            .unwrap_or_else(|| "HEAD".to_string());
 
         let github_api_base_url = std::env::var(EnvVars::UV_GITHUB_FAST_PATH_URL)
             .unwrap_or("https://api.github.com/repos".to_owned());
-        let github_api_url = format!("{github_api_base_url}/{owner}/{repo}/commits/{rev}");
+        let github_api_url = format!("{github_api_base_url}/{owner}/{repo}/commits/{reference}");
 
         debug!("Querying GitHub for commit at: {github_api_url}");
         let mut request = client.get(&github_api_url);

astral-sh/uv-dev

crates/uv-git/src/resolver.rs
@@ -128,11 +128,14 @@ impl GitResolver {
         }
 
         // Determine the Git reference.
-        let rev = url.reference().as_rev();
+        let reference = url
+            .reference()
+            .as_url_rev()
+            .unwrap_or_else(|| "HEAD".into());
 
         let github_api_base_url = std::env::var(EnvVars::UV_GITHUB_FAST_PATH_URL)
             .unwrap_or("https://api.github.com/repos".to_owned());
-        let github_api_url = format!("{github_api_base_url}/{owner}/{repo}/commits/{rev}");
+        let github_api_url = format!("{github_api_base_url}/{owner}/{repo}/commits/{reference}");
 
         debug!("Querying GitHub for commit at: {github_api_url}");
         let mut request = client.get(&github_api_url);