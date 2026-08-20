a0443a7ba6641175a795903f9c1690a235a16e98
Zanie Blue <contact@zanie.dev>
2026-08-20 10:04:52 -0500

    Preserve exact checksum authority artifact filenames

--- crates/uv-distribution/src/distribution_database.rs
+++ crates/uv-distribution/src/distribution_database.rs
@@ -781,7 +781,7 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
         let authority = self
             .client
             .unmanaged
-            .checksum_authority_record(index.map_or(&url, IndexUrl::url), &filename.to_string())
+            .checksum_authority_record(index.map_or(&url, IndexUrl::url), dist)
             .await?;
         let expected_size = match dist {
             BuiltDist::Registry(dist) if dist.best_wheel().size_is_authoritative => {
@@ -989,7 +989,7 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
         let authority = self
             .client
             .unmanaged
-            .checksum_authority_record(index.map_or(&url, IndexUrl::url), &filename.to_string())
+            .checksum_authority_record(index.map_or(&url, IndexUrl::url), dist)
             .await?;
         let expected_size = match dist {
             BuiltDist::Registry(dist) if dist.best_wheel().size_is_authoritative => {
