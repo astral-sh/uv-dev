astral-sh/uv

Reuse URL-provided hashes during resolution

crates/uv-distribution/src/distribution_database.rs
@@ -20,7 +20,7 @@ use uv_client::{
 use uv_distribution_filename::WheelFilename;
 use uv_distribution_types::{
     BuildInfo, BuildableSource, BuiltDist, Dist, DistRef, File, HashPolicy, Hashed, IndexUrl,
-    InstalledDist, Name, SourceDist, ToUrlError,
+    InstalledDist, Name, SourceDist, SourceUrl, ToUrlError,
 };
 use uv_extract::hash::Hasher;
 use uv_fs::write_atomic;
@@ -34,7 +34,7 @@ use uv_types::{BuildContext, BuildStack};
 
 use crate::archive::Archive;
 use crate::error::PythonVersion;
-use crate::hash::http_hash_algorithms;
+use crate::hash::{http_hash_algorithms, url_hashes};
 use crate::metadata::{ArchiveMetadata, Metadata};
 use crate::source::SourceDistributionBuilder;
 use crate::{Error, LocalWheel, Reporter, RequiresDist};
@@ -535,9 +535,16 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
         dist: &BuiltDist,
         hashes: HashPolicy<'_>,
     ) -> Result<ArchiveMetadata, Error> {
+        // Reuse declared URL hashes for resolution, without recording them in the artifact cache
+        // as computed hashes. The installer still validates the downloaded wheel.
+        let url_hashes = match (hashes, dist) {
+            (HashPolicy::Generate(_), BuiltDist::DirectUrl(dist)) => url_hashes(&dist.url),
+            _ => None,
+        };
+
         // If hash generation is enabled, and the distribution isn't hosted on a registry, get the
-        // entire wheel to ensure that the hashes are included in the response. If the distribution
-        // is hosted on an index, the hashes will be included in the simple metadata response.
+        // entire wheel unless the URL already provides a hash. If the distribution is hosted on
+        // an index, the hashes will be included in the simple metadata response.
         // For hash _validation_, callers are expected to enforce the policy when retrieving the
         // wheel.
         //
@@ -549,7 +556,7 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
         // not even be a compatible distribution!
         //
         // TODO(charlie): Request the hashes via a separate method, to reduce the coupling in this API.
-        if hashes.is_generate(dist) {
+        if hashes.is_generate(dist) && url_hashes.is_none() {
             let wheel = self.get_wheel(dist, hashes).await?;
             // If the metadata was provided by the user directly, prefer it.
             let metadata = if let Some(metadata) = self
@@ -574,7 +581,10 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
             .dependency_metadata()
             .get(dist.name(), Some(dist.version()))
         {
-            return Ok(ArchiveMetadata::from_metadata23(metadata));
+            return Ok(ArchiveMetadata {
+                metadata: Metadata::from_metadata23(metadata),
+                hashes: url_hashes.unwrap_or_else(HashDigests::empty),
+            });
         }
 
         let result = self
@@ -594,7 +604,10 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
         match result {
             Ok(metadata) => {
                 // Validate that the metadata is consistent with the distribution.
-                Ok(ArchiveMetadata::from_metadata23(metadata))
+                Ok(ArchiveMetadata {
+                    metadata: Metadata::from_metadata23(metadata),
+                    hashes: url_hashes.unwrap_or_else(HashDigests::empty),
+                })
             }
             Err(err) if err.is_http_streaming_unsupported() => {
                 warn!(
@@ -605,7 +618,7 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
                 // downloading the wheel directly, try that.
                 let wheel = self.get_wheel(dist, hashes).await?;
                 let metadata = wheel.metadata()?;
-                let hashes = wheel.hashes;
+                let hashes = url_hashes.unwrap_or(wheel.hashes);
                 Ok(ArchiveMetadata {
                     metadata: Metadata::from_metadata23(metadata),
                     hashes,
@@ -624,6 +637,16 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
         source: &BuildableSource<'_>,
         hashes: HashPolicy<'_>,
     ) -> Result<ArchiveMetadata, Error> {
+        let url_hashes = match (hashes, source) {
+            (HashPolicy::Generate(_), BuildableSource::Dist(SourceDist::DirectUrl(dist))) => {
+                url_hashes(&dist.url)
+            }
+            (HashPolicy::Generate(_), BuildableSource::Url(SourceUrl::Direct(source))) => {
+                url_hashes(source.url)
+            }
+            _ => None,
+        };
+
         // If the metadata was provided by the user directly, prefer it.
         if let Some(dist) = source.as_dist() {
             if let Some(metadata) = self
@@ -635,15 +658,27 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
                 // commits.
                 self.builder.resolve_revision(source, &self.client).await?;
 
-                return Ok(ArchiveMetadata::from_metadata23(metadata));
+                return Ok(ArchiveMetadata {
+                    metadata: Metadata::from_metadata23(metadata),
+                    hashes: url_hashes.unwrap_or_else(HashDigests::empty),
+                });
             }
         }
 
-        let metadata = self
+        // If resolving metadata requires a build, validate the declared hashes before executing
+        // the backend, even when the caller only requested hash generation.
+        let hashes = url_hashes
+            .as_ref()
+            .map_or(hashes, |hashes| HashPolicy::All(hashes.as_slice()));
+        let mut metadata = self
             .builder
             .download_and_build_metadata(source, hashes, &self.client)
             .boxed_local()
             .await?;
+
+        if let Some(hashes) = url_hashes {
+            metadata.hashes = hashes;
+        }
 
         Ok(metadata)
     }

astral-sh/uv-dev

crates/uv-distribution/src/distribution_database.rs
@@ -20,7 +20,7 @@ use uv_client::{
 use uv_distribution_filename::WheelFilename;
 use uv_distribution_types::{
     BuildInfo, BuildableSource, BuiltDist, Dist, DistHashPolicy, DistRef, File, Hashed, IndexUrl,
-    InstalledDist, Name, SourceDist, ToUrlError,
+    InstalledDist, Name, SourceDist, SourceUrl, ToUrlError,
 };
 use uv_extract::hash::Hasher;
 use uv_fs::write_atomic;
@@ -34,7 +34,7 @@ use uv_types::{BuildContext, BuildStack};
 
 use crate::archive::Archive;
 use crate::error::PythonVersion;
-use crate::hash::http_hash_algorithms;
+use crate::hash::{http_hash_algorithms, url_hashes};
 use crate::metadata::{ArchiveMetadata, Metadata};
 use crate::source::SourceDistributionBuilder;
 use crate::{Error, LocalWheel, Reporter, RequiresDist};
@@ -535,10 +535,16 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
         dist: &BuiltDist,
         hashes: DistHashPolicy<'_>,
     ) -> Result<ArchiveMetadata, Error> {
+        // Reuse declared URL hashes for resolution, without recording them in the artifact cache
+        // as computed hashes. The installer still validates the downloaded wheel.
+        let url_hashes = match (hashes, dist) {
+            (DistHashPolicy::Include, BuiltDist::DirectUrl(dist)) => url_hashes(&dist.url),
+            _ => None,
+        };
+
         // If hashes are requested during resolution, and the distribution isn't hosted on a
-        // registry, get the entire wheel to ensure that hashes are included in the response. If
-        // the distribution is hosted on an index, hashes are included in the simple metadata
-        // response.
+        // registry, get the entire wheel unless the URL already provides a hash. If the
+        // distribution is hosted on an index, hashes are included in the simple metadata response.
         // For hash _validation_, callers are expected to enforce the policy when retrieving the
         // wheel.
         //
@@ -550,7 +556,7 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
         // not even be a compatible distribution!
         //
         // TODO(charlie): Request the hashes via a separate method, to reduce the coupling in this API.
-        if hashes.needs_inclusion(dist) {
+        if hashes.needs_inclusion(dist) && url_hashes.is_none() {
             let wheel = self.get_wheel(dist, hashes).await?;
             // If the metadata was provided by the user directly, prefer it.
             let metadata = if let Some(metadata) = self
@@ -575,7 +581,10 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
             .dependency_metadata()
             .get(dist.name(), Some(dist.version()))
         {
-            return Ok(ArchiveMetadata::from_metadata23(metadata));
+            return Ok(ArchiveMetadata {
+                metadata: Metadata::from_metadata23(metadata),
+                hashes: url_hashes.unwrap_or_else(HashDigests::empty),
+            });
         }
 
         let result = self
@@ -595,7 +604,10 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
         match result {
             Ok(metadata) => {
                 // Validate that the metadata is consistent with the distribution.
-                Ok(ArchiveMetadata::from_metadata23(metadata))
+                Ok(ArchiveMetadata {
+                    metadata: Metadata::from_metadata23(metadata),
+                    hashes: url_hashes.unwrap_or_else(HashDigests::empty),
+                })
             }
             Err(err) if err.is_http_streaming_unsupported() => {
                 warn!(
@@ -606,7 +618,7 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
                 // downloading the wheel directly, try that.
                 let wheel = self.get_wheel(dist, hashes).await?;
                 let metadata = wheel.metadata()?;
-                let hashes = wheel.hashes;
+                let hashes = url_hashes.unwrap_or(wheel.hashes);
                 Ok(ArchiveMetadata {
                     metadata: Metadata::from_metadata23(metadata),
                     hashes,
@@ -625,6 +637,16 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
         source: &BuildableSource<'_>,
         hashes: DistHashPolicy<'_>,
     ) -> Result<ArchiveMetadata, Error> {
+        let url_hashes = match (hashes, source) {
+            (DistHashPolicy::Include, BuildableSource::Dist(SourceDist::DirectUrl(dist))) => {
+                url_hashes(&dist.url)
+            }
+            (DistHashPolicy::Include, BuildableSource::Url(SourceUrl::Direct(source))) => {
+                url_hashes(source.url)
+            }
+            _ => None,
+        };
+
         // If the metadata was provided by the user directly, prefer it.
         if let Some(dist) = source.as_dist() {
             if let Some(metadata) = self
@@ -636,15 +658,27 @@ impl<'a, Context: BuildContext> DistributionDatabase<'a, Context> {
                 // commits.
                 self.builder.resolve_revision(source, &self.client).await?;
 
-                return Ok(ArchiveMetadata::from_metadata23(metadata));
+                return Ok(ArchiveMetadata {
+                    metadata: Metadata::from_metadata23(metadata),
+                    hashes: url_hashes.unwrap_or_else(HashDigests::empty),
+                });
             }
         }
 
-        let metadata = self
+        // If resolving metadata requires a build, validate the declared hashes before executing
+        // the backend, even when the caller only requested hash resolution.
+        let hashes = url_hashes
+            .as_ref()
+            .map_or(hashes, |hashes| DistHashPolicy::All(hashes.as_slice()));
+        let mut metadata = self
             .builder
             .download_and_build_metadata(source, hashes, &self.client)
             .boxed_local()
             .await?;
+
+        if let Some(hashes) = url_hashes {
+            metadata.hashes = hashes;
+        }
 
         Ok(metadata)
     }