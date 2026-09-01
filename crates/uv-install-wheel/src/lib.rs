zaniebot/uv

Reject conflicting wheel scripts before install

crates/uv-install-wheel/src/lib.rs
@@ -15,7 +15,9 @@ pub use install::{install_wheel, installed_dist_info_path};
 pub use linker::{InstallState, LinkMode};
 pub use record::RecordEntry;
 pub use uninstall::{Uninstall, uninstall_egg, uninstall_legacy_editable, uninstall_wheel};
-pub use wheel::{WheelFile, read_record, read_record_into_iter, validate_and_heal_record};
+pub use wheel::{
+    WheelFile, read_record, read_record_into_iter, script_paths, validate_and_heal_record,
+};
 
 mod install;
 mod linker;

astral-sh/uv-dev

crates/uv-install-wheel/src/lib.rs
@@ -16,7 +16,8 @@ pub use linker::{InstallState, LinkMode};
 pub use record::RecordEntry;
 pub use uninstall::{Uninstall, uninstall_egg, uninstall_legacy_editable, uninstall_wheel};
 pub use wheel::{
-    WheelFile, read_record, read_record_into_iter, reserved_script_name, validate_and_heal_record,
+    WheelFile, read_record, read_record_into_iter, reserved_script_name, script_paths,
+    validate_and_heal_record,
 };
 
 mod install;