87ab4a83a64adb563f3de093ec22d3a18f3a322e
Zanie Blue <contact@zanie.dev>
2026-09-11 17:33:01 -0500

    Revert "Avoid allocating expanded compatibility tags (#20190)"
    This reverts commit 8cb9f394842ba9236850922d72d649d39a9b8a6f.

--- crates/uv-resolver/src/lock/mod.rs
+++ crates/uv-resolver/src/lock/mod.rs
@@ -7020,9 +7020,9 @@ impl WheelTagHint {
             .iter()
             .map(|filename| {
                 tags.compatibility(
-                    filename.python_tags().iter(),
-                    filename.abi_tags().iter(),
-                    filename.platform_tags().iter(),
+                    filename.python_tags(),
+                    filename.abi_tags(),
+                    filename.platform_tags(),
                 )
             })
             .max()?;
