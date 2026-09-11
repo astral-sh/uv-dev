fac53730a6be6f80a10565c177b22d252fbcd70d
Zanie Blue <contact@zanie.dev>
2026-09-10 23:17:09 -0500

    Add an experimental structured error report

--- crates/uv-errors/src/diagnostic.rs
+++ crates/uv-errors/src/diagnostic.rs
@@ -78,9 +78,9 @@ impl<'a> Diagnostic<'a> {
 
 /// Additional context, rather than a cause or an actionable hint.
 pub struct Info<'a> {
-    message: Cow<'a, str>,
-    details: Option<Cow<'a, str>>,
-    snippets: Vec<SourceSnippet<'a>>,
+    pub(crate) message: Cow<'a, str>,
+    pub(crate) details: Option<Cow<'a, str>>,
+    pub(crate) snippets: Vec<SourceSnippet<'a>>,
 }
 
 impl<'a> Info<'a> {
