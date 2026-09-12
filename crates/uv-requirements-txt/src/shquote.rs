31bd7aedde50282aa7a29f51b95022dd7f4388da
Zanie Blue <contact@zanie.dev>
2026-09-12 02:09:42 -0500

    Ablate requirements directive parsing fast paths

--- crates/uv-requirements-txt/src/shquote.rs
+++ crates/uv-requirements-txt/src/shquote.rs
@@ -142,20 +142,12 @@ fn unquote_open_escape(acc: &mut String, cursor: &mut std::iter::Enumerate<std::
 ///
 /// The result is canonical. There is only one valid unquoted result for a given input.
 ///
-/// If the string does not require any quoting or escaping, returns `Ok(None)`.
-///
 /// # Examples
 ///
 /// ```no_build
 /// assert_eq!(unquote("foobar").unwrap(), "foobar");
 /// ```
 pub(crate) fn unquote(source: &str) -> Result<Option<String>, UnquoteError> {
-    // If the string does not contain any single-quotes, double-quotes, or escape sequences, it
-    // does not require any unquoting.
-    if memchr::memchr3(b'\'', b'"', b'\\', source.as_bytes()).is_none() {
-        return Ok(None);
-    }
-
     // An unquote-operation never results in a longer string. Furthermore, the common case is
     // most of the string is unquoted / unescaped. Hence, we simply allocate the same space
     // for the resulting string as the input.
@@ -202,7 +194,7 @@ mod tests {
 
     #[test]
     fn basic() {
-        assert_eq!(unquote("foobar").unwrap(), None);
+        assert_eq!(unquote("foobar").unwrap().unwrap(), "foobar");
         assert_eq!(unquote("foo'bar'").unwrap().unwrap(), "foobar");
         assert_eq!(unquote("foo\"bar\"").unwrap().unwrap(), "foobar");
         assert_eq!(unquote("\\foobar\\").unwrap().unwrap(), "foobar");
