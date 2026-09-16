zaniebot/uv

Disambiguate checksumless Python download caches

crates/uv-python/src/downloads.rs
@@ -23,7 +23,7 @@ use tracing::{debug, instrument};
 use url::Url;
 
 use uv_cache::{Cache, CacheBucket};
-use uv_cache_key::cache_digest;
+use uv_cache_key::{CanonicalUrl, cache_digest};
 use uv_client::{
     BaseClient, BaseClientBuilder, CacheControl, CachedClient, CachedClientError, ClientBuildError,
     Connectivity, RetriableError, WrappedReqwestError, fetch_with_url_fallback,
@@ -1265,14 +1265,7 @@ impl ManagedPythonDownload {
         {
             let python_builds_dir = PathBuf::from(python_builds_dir);
             fs_err::create_dir_all(&python_builds_dir)?;
-            let hash_prefix = match self.sha256.as_deref() {
-                Some(sha) => {
-                    // Shorten the hash to avoid too-long-filename errors
-                    &sha[..9]
-                }
-                None => "none",
-            };
-            let target_cache_file = python_builds_dir.join(format!("{hash_prefix}-{filename}"));
+            let target_cache_file = python_builds_dir.join(self.cache_filename(&url, &filename));
 
             // Download the archive to the cache, or return a reader if we have it in cache.
             // TODO(konsti): We should "tee" the write so we can do the download-to-cache and unpacking
@@ -1414,6 +1407,18 @@ impl ManagedPythonDownload {
             })?;
 
         Ok(DownloadResult::Fetched(path))
+    }
+
+    fn cache_filename(&self, url: &DisplaySafeUrl, filename: &str) -> String {
+        let hash_prefix = match self.sha256.as_deref() {
+            // Shorten the hash to avoid too-long-filename errors.
+            Some(sha) => Cow::Borrowed(&sha[..9]),
+            None => Cow::Owned(format!(
+                "none-{}",
+                cache_digest(&CanonicalUrl::new(url.clone()))
+            )),
+        };
+        format!("{hash_prefix}-{filename}")
     }
 
     /// Download the managed Python archive into the cache directory.
@@ -1864,6 +1869,59 @@ mod tests {
     use uv_platform::{Arch, Libc, Os, Platform};
 
     use super::*;
+
+    #[test]
+    fn checksumless_custom_downloads_do_not_collide_in_cache() {
+        let parse_download = |url| {
+            let json = format!(
+                r#"{{"cpython-3.14.0-linux-x86_64-gnu":{{"name":"cpython","arch":{{"family":"x86_64","variant":null}},"os":"linux","libc":"gnu","major":3,"minor":14,"patch":0,"prerelease":null,"url":"{url}","sha256":null,"variant":null,"build":null}}}}"#
+            );
+            parse_json_downloads(
+                parse_downloads_json(json.as_bytes(), "downloads.json".to_string())
+                    .expect("custom downloads should parse"),
+            )
+            .pop()
+            .expect("custom download should be retained")
+        };
+
+        let first_url = DisplaySafeUrl::parse("https://example.com/first/python.tar.gz")
+            .expect("URL should parse");
+        let second_url = DisplaySafeUrl::parse("https://example.com/second/python.tar.gz")
+            .expect("URL should parse");
+        let first = parse_download(first_url.as_str());
+        let second = parse_download(second_url.as_str());
+
+        let first_cache_file = first.cache_filename(&first_url, "python.tar.gz");
+        let second_cache_file = second.cache_filename(&second_url, "python.tar.gz");
+        assert_ne!(first_cache_file, second_cache_file);
+        assert!(first_cache_file.starts_with("none-"));
+        assert!(second_cache_file.starts_with("none-"));
+        assert!(first_cache_file.ends_with("-python.tar.gz"));
+        assert!(second_cache_file.ends_with("-python.tar.gz"));
+
+        let equivalent_url =
+            DisplaySafeUrl::parse("https://user:password@example.com/first/%70ython.tar.gz")
+                .expect("URL should parse");
+        assert_eq!(
+            first.cache_filename(&equivalent_url, "python.tar.gz"),
+            first_cache_file
+        );
+    }
+
+    #[test]
+    fn python_download_cache_filename_preserves_checksum_key() {
+        let url =
+            DisplaySafeUrl::parse("https://example.com/python.tar.gz").expect("URL should parse");
+        let mut download = cpython_download_for_url("https://example.com/python.tar.gz");
+        download.sha256 = Some(Cow::Borrowed(
+            "c3223d5924a0ed0ef5958a750377c362d0957587f896c0f6c635ae4b39e0f337",
+        ));
+
+        assert_eq!(
+            download.cache_filename(&url, "python.tar.gz"),
+            "c3223d592-python.tar.gz"
+        );
+    }
 
     /// Parse a request with all of its fields.
     #[test]

astral-sh/uv-dev

crates/uv-python/src/downloads.rs
@@ -24,7 +24,7 @@ use tracing::{debug, instrument};
 use url::Url;
 
 use uv_cache::{Cache, CacheBucket};
-use uv_cache_key::cache_digest;
+use uv_cache_key::{CanonicalUrl, cache_digest};
 use uv_client::{
     BaseClient, BaseClientBuilder, CacheControl, CachedClient, CachedClientError, ClientBuildError,
     Connectivity, RetriableError, RetryState, WrappedReqwestError, fetch_with_url_fallback,
@@ -1266,14 +1266,7 @@ impl ManagedPythonDownload {
         {
             let python_builds_dir = PathBuf::from(python_builds_dir);
             fs_err::create_dir_all(&python_builds_dir)?;
-            let hash_prefix = match self.sha256.as_ref() {
-                Some(digest) => {
-                    // Shorten the hash to avoid too-long-filename errors
-                    &digest.as_str()[..9]
-                }
-                None => "none",
-            };
-            let target_cache_file = python_builds_dir.join(format!("{hash_prefix}-{filename}"));
+            let target_cache_file = python_builds_dir.join(self.cache_filename(&url, &filename));
 
             // Download the archive to the cache, or return a reader if we have it in cache.
             // TODO(konsti): We should "tee" the write so we can do the download-to-cache and unpacking
@@ -1415,6 +1408,18 @@ impl ManagedPythonDownload {
             })?;
 
         Ok(DownloadResult::Fetched(path))
+    }
+
+    fn cache_filename(&self, url: &DisplaySafeUrl, filename: &str) -> String {
+        let hash_prefix = match self.sha256.as_ref() {
+            // Shorten the hash to avoid too-long-filename errors.
+            Some(digest) => Cow::Borrowed(&digest.as_str()[..9]),
+            None => Cow::Owned(format!(
+                "none-{}",
+                cache_digest(&CanonicalUrl::new(url.clone()))
+            )),
+        };
+        format!("{hash_prefix}-{filename}")
     }
 
     /// Download the managed Python archive into the cache directory.
@@ -1867,6 +1872,57 @@ mod tests {
     use uv_platform::{Arch, Libc, Os, Platform};
 
     use super::*;
+
+    #[test]
+    fn checksumless_custom_downloads_do_not_collide_in_cache() {
+        let parse_download = |url| {
+            let json = format!(
+                r#"{{"cpython-3.14.0-linux-x86_64-gnu":{{"name":"cpython","arch":{{"family":"x86_64","variant":null}},"os":"linux","libc":"gnu","major":3,"minor":14,"patch":0,"prerelease":null,"url":"{url}","sha256":null,"variant":null,"build":null}}}}"#
+            );
+            parse_json_downloads(
+                parse_downloads_json(json.as_bytes(), "downloads.json".to_string())
+                    .expect("custom downloads should parse"),
+            )
+            .pop()
+            .expect("custom download should be retained")
+        };
+
+        let first_url = DisplaySafeUrl::parse("https://example.com/first/python.tar.gz")
+            .expect("URL should parse");
+        let second_url = DisplaySafeUrl::parse("https://example.com/second/python.tar.gz")
+            .expect("URL should parse");
+        let first = parse_download(first_url.as_str());
+        let second = parse_download(second_url.as_str());
+
+        let first_cache_file = first.cache_filename(&first_url, "python.tar.gz");
+        let second_cache_file = second.cache_filename(&second_url, "python.tar.gz");
+        assert_ne!(first_cache_file, second_cache_file);
+        assert!(first_cache_file.starts_with("none-"));
+        assert!(second_cache_file.starts_with("none-"));
+        assert!(first_cache_file.ends_with("-python.tar.gz"));
+        assert!(second_cache_file.ends_with("-python.tar.gz"));
+
+        let equivalent_url =
+            DisplaySafeUrl::parse("https://user:password@example.com/first/%70ython.tar.gz")
+                .expect("URL should parse");
+        assert_eq!(
+            first.cache_filename(&equivalent_url, "python.tar.gz"),
+            first_cache_file
+        );
+    }
+
+    #[test]
+    fn python_download_cache_filename_preserves_checksum_key() {
+        let url =
+            DisplaySafeUrl::parse("https://example.com/python.tar.gz").expect("URL should parse");
+        let mut download = cpython_download_for_url("https://example.com/python.tar.gz");
+        download.sha256 = Some(Digest::from_bytes([0xc3; 32]));
+
+        assert_eq!(
+            download.cache_filename(&url, "python.tar.gz"),
+            "c3c3c3c3c-python.tar.gz"
+        );
+    }
 
     /// Parse a request with all of its fields.
     #[test]