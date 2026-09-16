zaniebot/uv

Reject locks with incomplete dependency edges

crates/uv/src/commands/project/lock.rs
@@ -1436,6 +1436,18 @@ impl ValidatedLock {
                 }
                 Ok(Self::Preferable(lock))
             }
+            SatisfiesResult::MismatchedPackageDependencies(name, version, requirement) => {
+                if let Some(version) = version {
+                    debug!(
+                        "Resolving despite existing lockfile due to an uncovered dependency for: `{name}=={version}`\n  Requested: {requirement:?}"
+                    );
+                } else {
+                    debug!(
+                        "Resolving despite existing lockfile due to an uncovered dependency for: `{name}`\n  Requested: {requirement:?}"
+                    );
+                }
+                Ok(Self::Preferable(lock))
+            }
             SatisfiesResult::MismatchedPackageProvidesExtra(name, version, expected, actual) => {
                 if let Some(version) = version {
                     debug!(

astral-sh/uv-dev

crates/uv/src/commands/project/lock.rs
@@ -1581,6 +1581,18 @@ impl ValidatedLock {
                 }
                 Ok(Self::Preferable(lock))
             }
+            SatisfiesResult::UncoveredPackageDependency(name, version, requirement) => {
+                if let Some(version) = version {
+                    debug!(
+                        "Resolving despite existing lockfile due to an uncovered dependency for: `{name}=={version}`\n  Requested: {requirement:?}"
+                    );
+                } else {
+                    debug!(
+                        "Resolving despite existing lockfile due to an uncovered dependency for: `{name}`\n  Requested: {requirement:?}"
+                    );
+                }
+                Ok(Self::Preferable(lock))
+            }
             SatisfiesResult::MismatchedPackageProvidesExtra(name, version, expected, actual) => {
                 if let Some(version) = version {
                     debug!(