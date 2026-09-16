zaniebot/uv

Use a semantic enum for tree direction

crates/uv/src/commands/project/tree.rs
@@ -14,7 +14,7 @@ use uv_normalize::DefaultGroups;
 use uv_normalize::PackageName;
 use uv_preview::{Preview, PreviewFeature};
 use uv_python::{PythonDownloads, PythonPreference, PythonRequest, PythonVersion};
-use uv_resolver::{PackageMap, TreeDedupe, TreeDisplay, TreeJsonTarget};
+use uv_resolver::{PackageMap, TreeDedupe, TreeDirection, TreeDisplay, TreeJsonTarget};
 use uv_scripts::Pep723Script;
 use uv_settings::PythonInstallMirrors;
 use uv_warnings::warn_user;
@@ -49,7 +49,7 @@ pub(crate) async fn tree(
     prune: Vec<PackageName>,
     package: Vec<PackageName>,
     dedupe: TreeDedupe,
-    invert: bool,
+    direction: TreeDirection,
     outdated: bool,
     show_sizes: bool,
     python_version: Option<PythonVersion>,
@@ -309,7 +309,7 @@ pub(crate) async fn tree(
         &package,
         &groups,
         dedupe,
-        invert,
+        direction,
         show_sizes,
     );
 

astral-sh/uv-dev

crates/uv/src/commands/project/tree.rs
@@ -10,7 +10,7 @@ use uv_cli::TreeFormat;
 use uv_client::{BaseClientBuilder, RegistryClientBuilder};
 use uv_configuration::{ActiveEnvironment, Concurrency, DependencyGroups, TargetTriple};
 use uv_distribution_types::IndexCapabilities;
-use uv_lock::{PackageMap, TreeDedupe, TreeDisplay, TreeJsonTarget};
+use uv_lock::{PackageMap, TreeDedupe, TreeDirection, TreeDisplay, TreeJsonTarget};
 use uv_normalize::DefaultGroups;
 use uv_normalize::PackageName;
 use uv_preview::{Preview, PreviewFeature};
@@ -49,7 +49,7 @@ pub(crate) async fn tree(
     prune: Vec<PackageName>,
     package: Vec<PackageName>,
     dedupe: TreeDedupe,
-    invert: bool,
+    direction: TreeDirection,
     outdated: bool,
     show_sizes: bool,
     python_version: Option<PythonVersion>,
@@ -304,7 +304,7 @@ pub(crate) async fn tree(
         &package,
         &groups,
         dedupe,
-        invert,
+        direction,
         show_sizes,
     );
 